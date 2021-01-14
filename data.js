566a843393f1b2abbd5c1d46c3403ae3ed619b00
Kenneth Anderson <ken@kenneth.net.nz>
2021-01-15 10:30:26 +1300

    Implement loadjson function

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
+const fs = require('fs')
 
+module.exports = {
+  getUserById,
+  newUser,
+  editUser,
+  loadJson,
+  saveJson,
+}
 
-function getUserById(id, cb) {
+function getUserById(file, id, cb) {
 
 }
 
-function newUser(details, cb) {
+function newUser(file, details, cb) {
   
 }
 
@@ -13,7 +21,10 @@ function editUser(id, details, cb) {
 }
 
 function loadJson(file, cb) {
-  
+  fs.readFile(file, 'UTF-8', (err, data) => {
+    if (err) return cb(new Error(err.message))
+    cb(null, data)
+  })
 }
 
 function saveJson(file, cb) {
