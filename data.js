79fa1a47a3e1f497eea325403e454b7964dd5f32
Kenneth Anderson <ken@kenneth.net.nz>
2021-01-15 10:19:21 +1300

    set up first few functions

---
+++ data.js
@@ -0,0 +1,21 @@
+
+
+function getUserById(id, cb) {
+
+}
+
+function newUser(details, cb) {
+  
+}
+
+function editUser(id, details, cb) {
+  
+}
+
+function loadJson(file, cb) {
+  
+}
+
+function saveJson(file, cb) {
+  
+}
